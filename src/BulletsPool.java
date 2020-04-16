e900f475efbe4ed3bbbe8cc52353b180f494dfb5
kidstylex <chilizhiki@gmail.com>
2020-04-16 17:37:40 +0700

    BulletsPool

--- src/BulletsPool.java
+++ src/BulletsPool.java
@@ -21,4 +21,23 @@ public class BulletsPool {
         }
     }
 
+    public Bullet getReusable (int x, int y, int dx, int dy) {
+        if (BulletList.isEmpty()) {
+            for (int i = 0; i < 20; i++) {
+                BulletList.add(new Bullet(0 ,0,0,0));
+            }
+        }
+        Bullet setBullet = BulletList.get(0);
+        setBullet.setX(x);
+        setBullet.setY(y);
+        setBullet.setDx(dx);
+        setBullet.setDy(dy);
+        BulletList.remove(0);
+        return setBullet;
+    }
+
+    public void releaseReusable (Bullet bullet){
+        BulletList.add(bullet);
+    }
+
 }
